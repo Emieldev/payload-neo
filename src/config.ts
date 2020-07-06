9adcd0b5b7e2408525a8a88038991d8741755819
c43721 <55610086+c43721@users.noreply.github.com>
2020-07-06 14:58:37 -0500

    Docker Update

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+const config = {
+    PREFIX: "pls ",
+    allowedID: ["176457969465163776"],
+
+    files: {
+        LOGS_CSS: __dirname + "/assets/dark-logs.css",
+    },
+
+    GOOGLE_CREDENTIALS_PATH: __dirname + "../googledata.json",
+}
+export default config;
